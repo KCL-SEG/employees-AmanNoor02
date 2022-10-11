68b7d3bbca3ee3150f6a96bc014f479ca2ef3e4e
Aman Noor <aman.noor@kcl.ac.uk>
2022-10-11 15:14:21 +0100

    Revamp whole code into if statements instead of functions

--- employee.py
+++ employee.py
@@ -2,53 +2,61 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, salary, hours, type_of_contract, type_of_commission, commission_salary, number_of_contracts):
+    def __init__(self, name, salary = 0, hours = 0, hourlyPay = 0, commissionPay = 0, number_of_contracts = 0, bonus = 0):
         self.name = name
         self.salary = salary
         self.hours = hours
-        self.type_of_contract = type_of_contract
-        self.type_of_commission = type_of_commission
-        self.commission_salary = commission_salary
+        self.hourlyPay = hourlyPay
+        self.commissionPay = commissionPay
         self.number_of_contracts = number_of_contracts
+        self.bonus = bonus
 
     def get_pay(self):
-        pay = calculateSalary(self.type_of_contract, self.hours) + calculateCommission(self.commission_salary,self.number_of_contracts)
-        return pay
+        if self.salary:
+            self.salaryPay += self.salary
+        if self.hours:
+            self.salaryPay += (self.hours * self.hourlyPay)
+        if self.number_of_contracts:
+            self.salaryPay += (self.number_of_contracts * self.commissionPay)
+        if self.bonus:
+            self.salaryPay += self.bonus
+        return self.salaryPay
 
     def __str__(self):
-        return self.name
+        salaryMessage = self.name
 
-    def calculateSalary(type_of_contract, hours):
-        if type_of_contract == "monthly":
-            salary_pay = self.salary
+        if self.salary:
+            salaryMessage += (f" works on a monthly salary of {self.salary}")
         else:
-            salary_pay = self.salary * self.hours
-        return salary_pay
+            (f"works on a contract of {self.hours} hours at {self.hourlyPay}/hour")
 
-    def calculateCommission(commission_salary,number_of_contracts):
-        if type_of_commission == "fixed":
-            commissionPay = commission_salary
-        else:
-            commissionPay = commission_salary * number_of_contracts
-        return commissionPay
+        if self.number_of_contracts:
+            salaryMessage += (f"and receives a commission for {self.number_of_contracts} contract(s) at {self.commissionPay}/contract.")
+
+        if self.bonus:
+            salaryMessage += (f" and receives a bonus commission of {self.bonus}.")
+
+        if not self.number_of_contracts and not self.bonus:
+            salaryMessage += (".")
+            
+        salaryMessage += (f" Their total pay is {self.salaryPay}.")
+        return salaryMessage
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie', '4000', 'none', 'monthly', 'none', 'none', 'none')
+billie = Employee('Billie', salary = 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie', '25', '100', 'hourly','none', 'none', 'none')
+charlie = Employee('Charlie', hours = 100, hourlyPay = 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee', '3000', 'none', 'monthly', 'contracted', '200', '4')
+renee = Employee('Renee', salary = 3000, commissionPay = 200, number_of_contracts = 4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan', '25', '150', 'hourly', 'contracted', '220', '3')
+jan = Employee('Jan', hours = 150, hourlyPay = 25, commissionPay = 220, number_of_contracts = 3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie', '2000', 'none', 'monthly', 'fixed', '1500', 'none')
+robbie = Employee('Robbie', salary = 2000, bonus = 1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel', '30', '120', 'hourly', 'fixed', '600', 'none')
-
-print (get_pay(billie))
+ariel = Employee('Ariel', hours = 120, hourlyPay = 30, bonus = 600)
